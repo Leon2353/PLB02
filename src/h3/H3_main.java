ad30aadad197f9f0e4ce5acb12290886e3a91838
Leon2353 <lboeckm2@smail.uni-koeln.de>
2025-10-30 16:24:05 +0100

    Aenderung P3_main (Korrekt verschachtelt)

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -1,47 +1,38 @@
 package h3;
 
 public class H3_main {
-	
-	public static void main (String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		int i = -3000;
 		int j = 300;
 		int k = -10;
-		
-		if (i>j && i<=200 && j<=100) {
-			
-			k=1;
-			System.out.println("k lautet: " + k);
-			
+
+		if (i > j) {
+
+			k = 1;
+
+			if (i > 200) {
+
+				k = 2;
+
+				if (j > 100) {
+
+					k = 3;
+
+				}
+			}
+
 		}
 
-		else if (i>j && i>200 && j<=100) {
-			
-			k=2;
-			System.out.println("k lautet: " + k);
-			
+		else if (i <= j && i <= 200 && j <= 100) {
+
+			k = 4;
+
 		}
 		
-		else if (i>j && i>200 && j>100) {
-			
-			k=3;
-			System.out.println("k lautet: " + k);
-			
-		}
-		
-		else if (i<=j && i<=200 && j<=100) {
-			
-			k=4;
-			System.out.println("k lautet: " + k);
-			
-		}
-		
-		else {
-			
-			System.out.println("k lautet: " + k);
-			
-		}
-		
+		//System.out.println("k lautet: " + k);
+
 	}
 
 }
