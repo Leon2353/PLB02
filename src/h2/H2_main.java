77d23195159eec15b0f949fe82735fcb0e316641
Leon2353 <lboeckm2@smail.uni-koeln.de>
2025-10-30 16:29:05 +0100

    Aenderung P2_main, unnoetige Ausgaben geloescht

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,55 +1,49 @@
 package h2;
 
 public class H2_main {
-	
-	public static void main (String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		int i = -100;
 		int j = 2;
 		int k = 7;
 		int min = 0;
 		int max = 0;
-		
-		if (i>j && i>k) {
-			max=i;
-			System.out.println("Das Maximum liegt bei: " + max);
+
+		if (i > j && i > k) {
+			
+			max = i;
+			
 		}
-		
-		else if (j>i && j>k) {
-			max=j;
-			System.out.println("Das Maximum liegt bei: " + max);
+
+		else if (j > i && j > k) {
+			
+			max = j;
+			
 		}
-		
-		else if (k>i && k>j) {
-			max=k;
-			System.out.println("Das Maximum liegt bei: " + max);
+
+		else if (k > i && k > j) {
+			
+			max = k;
+			
 		}
-		/*
-		//Nur falls i,j,k nicht paarweise verschieden.
-		else {
-			System.out.println("Kein eindeutiges Maximum.");
+
+		if (i < j && i < k) {
+			
+			min = i;
+			
+		} else if (j < i && j < k) {
+			
+			min = j;
+			
+		} else if (k < i && k < j) {
+			
+			min = k;
+
 		}
-		*/
-		
-	
-		if (i<j && i<k) {
-			min=i;
-			System.out.println("Das Minimum liegt bei: " + min);
-		}
-		else if (j<i && j<k) {
-			min=j;
-			System.out.println("Das Minimum liegt bei: " + min);
-		}
-		else if (k<i && k<j) {
-			min=k;
-			System.out.println("Das Minimum liegt bei: " + min);
-		}
-		/*
-		//Nur falls i,j,k nicht paarweise verschieden.
-		 else {
-			System.out.println("Kein eindeutiges Minimum.");
-		}
-		*/
+
+		System.out.println("Das Maximum liegt bei: " + max);
+		System.out.println("Das Minimum liegt bei: " + min);
 	}
-	
+
 }
